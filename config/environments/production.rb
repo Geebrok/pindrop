256892fadc7866eb775be93088d9853ee3aa578a
grant brooks <grantbrooks75@gmail.com>
2015-09-08 20:46:41 +1000

    added files to gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,4 @@ Rails.application.configure do
   # Required for Heroku
   # Set this to "host" to web address and remove port 3000
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  # Sets paperclip to upload images to Amazon s3
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
 end
